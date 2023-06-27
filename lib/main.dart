779ec5dcc7f2792a2d4ef1fb2b807ccd920b0217
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-06-27 14:21:47 +0330

    Optimize Startscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 85, 17, 97),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 250,
-              ),
-              const SizedBox(height: 80),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 85, 17, 97),
-                ),
-                child: const Text(
-                  'Start Quiz',
-                ),
-              ),
-            ],
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 49, 8, 88),
+                Color.fromARGB(255, 83, 7, 99),
+              ],
+            ),
           ),
+          child: const StartScreeen(),
         ),
       ),
     ),
