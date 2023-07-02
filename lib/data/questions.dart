5bfbe727e64fa4ef17c689ee637b640b7335814a
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-02 21:53:11 +0330

    Create Question Model

---
+++ lib/data/questions.dart
@@ -0,0 +1,13 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    "What are the main building blocks of flutter UIs?",
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  )
+];
